115bc15899c3614505c4211813800cd2c8107b1b
Luis Bravo <bravoluis439@ymail.com>
2023-09-29 14:31:38 -0300

    continuo con el proyecto

--- javascript.js
+++ javascript.js
@@ -1,12 +1,14 @@
 // Esto sera una página que vende gaseosas
 
-function bienvenidaCliente() {
-  alert(`Les damos la bienvenida a nuestra tienda de gaseosas`);
-}
+// function bienvenidaCliente() {
+//   alert(`Les damos la bienvenida a nuestra tienda de gaseosas`);
+// }
 
-bienvenidaCliente();
+// bienvenidaCliente();
 
-function Gaseosas(gaseosa1id, gaseosa2id, gaseosa3id, gaseosa4id) {
+ href="./pages/gaseosascola.html"
+
+function Marcasgaseosas(gaseosa1id, gaseosa2id, gaseosa3id, gaseosa4id) {
   this.gaseosa1 = gaseosa1id;
   this.gaseosa2 = gaseosa2id;
   this.gaseosa3 = gaseosa3id;
@@ -14,13 +16,13 @@ function Gaseosas(gaseosa1id, gaseosa2id, gaseosa3id, gaseosa4id) {
 }
 // con la función que cree arriba, empiezo a hacer varios objetos, solo voy cambiando el atributo
 
-const gaseosasCola = new Gaseosas("Cocacola", "Pepsi", "Manaos", "La bichy");
+const gaseosasCola = new Marcasgaseosas("Cocacola", "Pepsi", "Manaos", "La bichy");
 console.log(gaseosasCola);
 
-const gaseosasNaranja = new Gaseosas("Fanta", "Manaos", "Crush");
+const gaseosasNaranja = new Marcasgaseosas("Fanta", "Manaos", "Crush");
 console.log(gaseosasNaranja);
 
-const gaseosasPomelo = new Gaseosas("Crush", "Schweppes", "Manaos", "Secco");
+const gaseosasPomelo = new Marcasgaseosas("Crush", "Schweppes", "Manaos", "Secco");
 console.log(gaseosasPomelo);
 
 // hago arrays para especificar que marca quiere el usuario (le especifico el gusto entre parentesis,asi cuando van al carrito, y hay por ejemplo 2 marcas iguales, se puedan diferenciar los gustos :D)
@@ -42,6 +44,98 @@ const arrayGaseosasPomelo = [
   "Secco(pomelo)",
 ];
 
+
+// Funciones exclusivas para los gustos de gaseosas del DOM!
+class GaseosasdeCola {
+constructor (id, nombre, precio, litros,imagen) {
+      
+     this.id = id;
+     this.nombre = nombre;
+     this.precio = precio;
+     this.litros = litros;
+     this.imagen = imagen;
+  
+}
+}
+//Instanciación de objetos: 
+const cola1 = new GaseosasdeCola(1,"Coca cola - Sabor original", 898, "2,5L","00180538.jpg" )
+const cola2 = new GaseosasdeCola(2,"Pepsi", 694, "3L","00207054.jpg")
+
+// capturo por medio del DOM
+let colas = document.getElementById("gaseosasCola2");
+let naranjas = document.getElementById("gaseosasCola2");
+let pomelo = document.getElementById("gaseosasCola2");
+let paginaCola=document.getElementById("gaseosasCola3");
+
+// Aaca imprimi las cosas que se ven en el index.html con el DOM
+
+let carCola = document.createElement("div"); 
+carCola.className = "col-12 col-md-6 col-lg-4 my-2"
+ carCola.innerHTML = ` <div id="1" class="card" style="width: 18rem;">
+                    <img class="card-img-top img-fluid" style="height: 200px;"src="imagenes/ley-de-7NWNSWWLGJBKBA3QTB7QT4KAS4.webp" alt="Gaseosas de cola">
+                    <div class="card-body">
+                        <h4 class="card-title"></h4>
+                        <a  href="./pages/gaseosascola.html" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Ver marcas de gusto Cola</a>
+                     </div>
+     </div> `
+
+colas.append(carCola)
+
+
+let carNaranja = document.createElement("div"); 
+carNaranja.className = "col-12 col-md-6 col-lg-4 my-2"
+ carNaranja.innerHTML = ` <div id="1" class="card" style="width: 18rem;">
+                    <img class="card-img-top img-fluid" style="height: 200px;"src="imagenes/crush-nja1-a94ddde97a18c41d8516004366860968-1024-1024.jpeg" alt="Gaseosas de cola">
+                    <div class="card-body">
+                        <h4 class="card-title"></h4>   
+                        <a  href="./pages/gaseosascola.html" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Ver marcas de gusto Naranja</a>
+                     </div>
+     </div> `
+
+naranjas.append(carNaranja)
+
+
+let carPomelo = document.createElement("div"); 
+carPomelo.className = "col-12 col-md-6 col-lg-4 my-2"
+ carPomelo.innerHTML = ` <div id="1" class="card" style="width: 18rem;">
+                    <img class="card-img-top img-fluid" style="height: 200px;"src="imagenes/D_NQ_NP_639394-MLA69983179596_062023-O.webp" alt="Gaseosas de cola">
+                    <div class="card-body">
+                        <h4 class="card-title"></h4> 
+                        <a  href="./pages/gaseosascola.html" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Ver marcas de gusto Pomelo</a>
+                     </div>
+     </div> `
+
+pomelo.append(carPomelo)
+
+
+// Aca imprimo lo que se ve en gaseosascola.html con el DOM
+function mostrarGustosCola(array){
+  //for of: para recorrer un array posición a posición
+  for(let GaseosasdeCola of array){
+let mostrargaseosascolaDiv = document.createElement("div")
+    mostrargaseosascolaDiv.innerHTML = ` <div id="${GaseosasdeCola.id}" class="card" style="width: 18rem;">
+    <img class="card-img-top img-fluid" style="height: 200px;"src="imagenes/D_NQ_NP_639394-MLA69983179596_062023-O.webp" alt="Gaseosas de cola">
+    <div class="card-body">
+        <h4 class="card-title"></h4> 
+        <a  href="./pages/gaseosascola.html" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Ver marcas de gusto Pomelo</a>
+     </div>
+</div> `
+paginaCola.append(mostrargaseosascolaDiv)
+
+  }
+}
+
+
+const analizarEleccion = (opcionCola, array) => {
+  let eleccion = array.find((prod, index) => {
+    if (opcionCola === index + 1){
+      
+      return prod
+    }
+   })  
+  return eleccion
+};
+
 // creo este array para guardar todo lo que pidio el cliente
 const carrito = [];
 
@@ -57,27 +151,39 @@ function menu() {
     );
     switch (opcionIngresada) {
       case 1:
-        Gaseosas();
         console.log(gaseosasCola);
-       let opcionCola =  parseInt(prompt(`Porfavor ingrese que marca de gaseosa gusto "Cola", desea comprar:
+        let opcionCola = parseInt(
+          prompt(`Porfavor ingrese que marca de gaseosa gusto "Cola", desea comprar:
               1 - Coca cola 
               2 - Pepsi 
               3 - Manaos 
               4 - La bichy  `)
-              );
-
+        );
+        alert(analizarEleccion(opcionCola, arrayGaseosasCola));
+        carrito.push()
         break;
 
       case 2:
-        Gaseosas();
         console.log(gaseosasNaranja);
-        
+        let opcionNaranja = parseInt(
+          prompt(`Porfavor ingrese que marca de gaseosa gusto "Naranja", desea comprar:
+              1 - Fanta 
+              2 - Manaos
+              3 - Crush `)
+        );
+        alert(analizarEleccion(opcionNaranja, arrayGaseosasNaranja));
         break;
 
       case 3:
-        Gaseosas();
         console.log(gaseosasPomelo);
-        
+        let opcionPomelo = parseInt(
+          prompt(`Porfavor ingrese que marca de gaseosa gusto "Pomelo", desea comprar:
+              1 - Crush 
+              2 - Schweppes
+              3 - Manaos 
+              4 - Secco `)
+        );
+        alert(analizarEleccion(opcionPomelo, arrayGaseosasPomelo));
         break;
       case 0:
         console.log(
@@ -93,14 +199,14 @@ function menu() {
     }
   } while (!salirMenu);
 }
-menu();
+// menu();
 
 // function arrayCola() {
 //   let opcionIngresada2 = parseInt(
 //     prompt(`Porfavor ingrese que marca de gaseosa gusto "Cola", desea comprar:
-//     1 - Coca cola 
-//     2 - Pepsi 
-//     3 - Manaos 
+//     1 - Coca cola
+//     2 - Pepsi
+//     3 - Manaos
 //     4 - La bichy  `)
 //   );
 //   switch (opcionIngresada2) {
